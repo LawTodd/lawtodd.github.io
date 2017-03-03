83514ed6cd1d4e200c6b8ffe0f6846a9d7be1b82
Todd Law <lawtodd1@gmail.com>
2017-03-03 15:19:23 -0500

    working repo links

--- js/blog.js
+++ js/blog.js
@@ -1,26 +1,24 @@
 $(document).ready(function(){
 	$.get("https://api.github.com/users/lawtodd",
 		function(data,status){
-		$.each(data,function(i,item){
-			if(i=="repos_url"){
-				$.get(item,function(data,status){
-					console.log("data:");
-					console.log(data);
-					var projectLink;
-					var projectName;
-					var projectDate;
-					$.each(data, function(i,item){
-						var aRepo = data[i];
-						var	projectName = aRepo.name;
-						var	projectLink = aRepo.html_url;
-						var	projectDate = aRepo.updated_at;	
-						$("ul.list-unstyled").append(
+			console.log("data:");
+			console.log(data);
+			var item = data.repos_url;
+			$.get(item,function(data,status){
+				console.log("data:");
+				console.log(data);
+				var projectLink;
+				var projectName;
+				var projectDate;
+				$.each(data, function(i,item){
+					var aRepo = data[i];
+					var	projectName = aRepo.name;
+					var	projectLink = aRepo.html_url;
+					var	projectDate = aRepo.updated_at;	
+					$("ul.list-unstyled").append(
 						'<li><a href="' + projectLink + '">' + projectName +
 						'</a><p>'+ projectDate +'</p></li>');
-					});
-						
-				})
-			}
-		})
+					});	
+			})
 	})
 });
