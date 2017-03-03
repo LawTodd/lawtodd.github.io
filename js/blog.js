818ad7a0981deb4416b447342e8b9731d891af99
Todd Law <lawtodd1@gmail.com>
2017-03-03 13:34:42 -0500

    nested gets

--- js/blog.js
+++ js/blog.js
@@ -1,18 +1,29 @@
 $(document).ready(function(){
 	$.get("https://api.github.com/users/lawtodd",
-		function(data,status);
-	alert("Data: " + data + "\nStatus: " + status);
-	getRepos.function(){
-		$.get(repos_url,formatRepos)
-	}
-	formatRepos.function(){
-		for ("name" in data) {
-			var projectName = name;
-			var link = url;
-			var date = updated_at;
+		function(data,status){
+		alert("Data: " + data + "\nStatus: " + status)
+		$.each(data,function(i,item){
+			if(i=="repos_url"){
+				$.get(item,function(data,status){
+					alert("Data: " + data + "\nStatus: " + status)
+					$.each(data, function(i,item){
+						if(i=="name") {
+							projectName = item;
+						} else {
+							if(i=="html_url"){
+								link = item;
+						} else {
+							if(i=="updated_at"){
+								date = item;	
+								}
+							}
+						}
+					})
+				})
 			$("ul").append(
 				'<li><a href="' + link + '">' +projectName+
-				'</a><p>'+date+'</p></li>');
-		}
-	}
+				'</a><p>'+date+'</p></li>');	
+			}
+		})
+	})
 });
